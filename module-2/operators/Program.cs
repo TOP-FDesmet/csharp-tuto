cd7d00d0ca43d67feb980e8c19c27d3373d007ea
TOP-FDesmet <florian.desmet@topchretien.com>
2025-02-26 16:52:49 +0100

    operators

---
+++ module-2/operators/Program.cs
@@ -0,0 +1,29 @@
+﻿int num1 = 9;
+int num2 = 2;
+
+int result = num1 + num2;
+Console.WriteLine($"Addition = {result}");
+
+result = num1 - num2;
+Console.WriteLine($"Soustraction = {result}");
+
+result = num1 * num2;
+Console.WriteLine($"Multiplication = {result}");
+
+result = num1 / num2;
+Console.WriteLine($"Division = {result}");
+
+result = num1 % num2;
+Console.WriteLine($"Modulo = {result}");
+
+string firstname;
+int age;
+
+Console.WriteLine("Veuillez saisir votre prénom");
+firstname = Console.ReadLine();
+
+Console.WriteLine("Veuillez saisir votre âge");
+age = Int32.Parse(Console.ReadLine());
+
+// String operator
+Console.WriteLine("Bonjour " + firstname + ", vous avez " + age + " ans !");
