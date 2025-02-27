775e85e2ec66ac8b85a926460f283755a932eabc
TOP-FDesmet <florian.desmet@topchretien.com>
2025-02-27 12:17:35 +0100

    create mistery number game

---
+++ MisteryNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿const int misteryNumber = 4;
+string firstname;
+int yourNumber;
+
+Console.WriteLine("Veuillez saisir votre prénom.");
+firstname = Console.ReadLine();
+
+Console.WriteLine($"Bienvenue {firstname} ! Veuillez saisir un nombre entre 1 et 7.");
+yourNumber = int.Parse(Console.ReadLine());
+
+if (yourNumber == misteryNumber)
+{
+  Console.WriteLine("Bravo vous avez gagné !");
+}
+else
+{
+  Console.WriteLine("Oups ! Vous avez perdu.");
+  
+  if (yourNumber > misteryNumber)
+  {
+    Console.WriteLine("Le nombre mystère saisi est plus petit !");
+  }
+  else
+  {
+    Console.WriteLine("Le nombre mystère saisi est plus grand !");
+  }
+}
