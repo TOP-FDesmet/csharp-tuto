e31e964d534d2089c7dd7643a6ba3e93af7a1437
TOP-FDesmet <florian.desmet@topchretien.com>
2025-02-27 18:12:49 +0100

    update with foreach

--- MisteryNumber/Program.cs
+++ MisteryNumber/Program.cs
@@ -1,27 +1,56 @@
 ﻿const int MISTERY_NUMBER = 4;
 string firstname;
 int yourNumber;
+List<int> numbersPlayed = [];
 
 Console.WriteLine("Veuillez saisir votre prénom.");
 firstname = Console.ReadLine();
 
-Console.WriteLine($"Bienvenue {firstname} ! Veuillez saisir un nombre entre 1 et 7.");
-yourNumber = int.Parse(Console.ReadLine());
+string indication = "";
+bool win = false;
 
-if (yourNumber == MISTERY_NUMBER)
+while (!win)
 {
-  Console.WriteLine("Bravo vous avez gagné !");
-}
-else
-{
-  Console.WriteLine("Oups ! Vous avez perdu.");
-  
-  if (yourNumber > MISTERY_NUMBER)
-  {
-    Console.WriteLine("Le nombre mystère saisi est plus petit !");
-  }
-  else
-  {
-    Console.WriteLine("Le nombre mystère saisi est plus grand !");
-  }
+    Console.Clear();
+
+    if (numbersPlayed is not [])
+    {
+        Console.Write("Nombres joués : ");
+
+        foreach (int num in numbersPlayed)
+        {
+            Console.Write($" {num} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    if (indication != "")
+    {
+        Console.WriteLine(indication);
+    }
+
+    Console.WriteLine($"Bienvenue {firstname} ! Veuillez saisir un nombre entre 1 et 10.");
+    yourNumber = int.Parse(Console.ReadLine());
+
+    numbersPlayed.Add(yourNumber);
+
+    if (yourNumber == MISTERY_NUMBER)
+    {
+        Console.WriteLine("Bravo vous avez gagné !");
+        win = true;
+    }
+    else
+    {
+        Console.WriteLine("Oups ! Vous avez perdu.");
+
+        if (yourNumber > MISTERY_NUMBER)
+        {
+            indication = "Le nombre mystère saisi est plus petit !";
+        }
+        else
+        {
+            indication = "Le nombre mystère saisi est plus grand !";
+        }
+    }
 }
---
+++ module-2/loop/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] values = new int[3];
+
+for (int i = 0; i < values.Length; i++)
+{
+    Console.WriteLine($"Veuillez saisir la valeur {i + 1}!");
+    values[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < values.Length; i++)
+{
+    Console.WriteLine($"La valeur {i + 1} est {values[i]}");
+}
