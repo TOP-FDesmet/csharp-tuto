b4b694bd6b6812b07d0e392390de253827933438
TOP-FDesmet <florian.desmet@topchretien.com>
2025-03-13 16:40:18 +0100

    create console menu

--- PassManager/Program.cs
+++ PassManager/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] menuItems = 
+[
+    "Add password",
+    "Consulter password",
+    "Exit"
+];
+
+int index = 0;
+ConsoleKeyInfo key;
+
+do
+{
+    Console.Clear();
+    foreach (var (idx, menuItem) in menuItems.Index())
+    {
+        if (idx == index)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        Console.WriteLine(menuItem);
+        Console.ResetColor();
+    }
+    
+    key = Console.ReadKey();
+
+    if (key.Key == ConsoleKey.UpArrow)
+    {
+        index = Math.Max(0, index - 1);
+    } 
+    else if (key.Key == ConsoleKey.DownArrow)
+    {
+        index = Math.Min(menuItems.Length - 1, index + 1);
+    }
+    else if (key.Key == ConsoleKey.Enter)
+    {
+        var choice = menuItems[index];
+        Console.WriteLine(choice);
+        Console.WriteLine("Press enter to continue...");
+        Console.ReadLine();
+    }
+    
+} while (key.Key != ConsoleKey.Escape);
+
+Console.WriteLine("Program ending...");
